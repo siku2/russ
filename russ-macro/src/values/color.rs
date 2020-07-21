3bf844be0e99577b4e6f01efb4fad120ea4367c3
Simon Berger <simon@siku2.io>
2020-07-22 00:46:15 +0200

    support for colors

--- russ-macro/src/values/color.rs
+++ russ-macro/src/values/color.rs
@@ -1,16 +1,29 @@
 use super::numeric::{Number, Percentage};
 use russ_internal_macro::vds;
 
-vds! {
-    <"color"> = <hex-color> | <named-color> | currentcolor | transparent
-                <rgb-color> | <rgba-color> | <hsl-color> | <hsla-color> | <hwb-color> |
-                <lab-color> | <lch-color> | <gray-color> |
-                <color-color> | <device-cmyk-color> |
-                <system-color>;
-}
+// TODO vds needs to support grouping using () natively.
 
 vds! {
-    <"rgb-color"> = "rgb(" <percentage>{3} [ "/" <alpha-value> ]? ")" |
-                    "rgb(" <number>{3} [ "/" <alpha-value> ]? ")";
+    <"color"> = <hex-color> | <named-color> | currentcolor | transparent |
+                <rgb-fn> | <hsl-fn> | <hwb-fn> |
+                <lab-fn> | <lch-fn> | <gray-fn> |
+                <color-fn> | <device-cmyk-fn> |
+                <system-color>;
+
+    // common values
     <alpha-value> = <number> | <percentage>;
+    <hue> = <number> | <angle>;
+
+    <rgb-fn> = "rgb"( <percentage>{3} [ "/" <alpha-value> ]? ) |
+               "rgb"( <number>{3} [ "/" <alpha-value> ]? );
+    <hsl-fn> = "hsl"( <hue> <percentage> <percentage> [ "/" <alpha-value> ]? );
+    <hwb-fn> = "hwb"( <hue> <percentage> <percentage> [ "/" <alpha-value> ]? );
+
+    <lab-fn> = "lab"( <percentage> <number> <number> [ "/" <alpha-value> ]? );
+    <lch-fn> = "lch"( <percentage> <number> <hue> [ "/" <alpha-value> ]? );
+    <gray-fn> = "gray"( <number>  [ "/" <alpha-value> ]? );
+
+    <color-fn> = "color"( [ <css-ident>? [ <number>+ | <css-string> ] [ "/" <alpha-value> ]? ]# , <color>? );
+    <device-cmyk-fn> = "device-cmyk"( <cmyk-component>{4} [ "/" <alpha-value> ]? , <color>? );
+    <cmyk-component> = <number> | <percentage>;
 }
