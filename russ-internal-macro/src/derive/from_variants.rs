f099ae605641fed4bce952a4bd5feff64e06a305
Simon Berger <simon@siku2.io>
2020-08-11 02:07:20 +0200

    make fromvariants generic

--- russ-internal-macro/src/derive/from_variants.rs
+++ russ-internal-macro/src/derive/from_variants.rs
@@ -1,7 +1,9 @@
 use super::args::{self, Args, FromArgs};
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{spanned::Spanned, Attribute, Data, DeriveInput, Fields, Ident, Variant};
+use syn::{
+    parse_quote, spanned::Spanned, Attribute, Data, DeriveInput, Fields, Generics, Ident, Variant,
+};
 
 struct FromVariantAttr {
     attr: Attribute,
@@ -25,13 +27,18 @@ impl ToTokens for FromVariantAttr {
     }
 }
 
-fn generate_from_variant(type_ident: &Ident, variant: &Variant) -> syn::Result<TokenStream> {
+fn generate_from_variant(
+    input: &DeriveInput,
+    type_ident: &Ident,
+    variant: &Variant,
+) -> syn::Result<TokenStream> {
+    let mut generics: Generics = input.generics.clone();
+
     let variant_ident = &variant.ident;
 
     let arg: Option<FromVariantAttr> = args::parse_single_from_attrs(&variant.attrs).transpose()?;
     let gen_into = arg.map(|arg| arg.into).unwrap_or_default();
 
-    let mut type_constraints = Vec::new();
     let mut types = Vec::new();
     let mut bind_idents = Vec::new();
     let mut use_idents = Vec::new();
@@ -40,7 +47,11 @@ fn generate_from_variant(type_ident: &Ident, variant: &Variant) -> syn::Result<T
         if gen_into {
             let ty_n = Ident::new(&format!("T{}", i), ty.span());
             types.push(ty_n.to_token_stream());
-            type_constraints.push(quote! { #ty_n: Into<#ty> });
+
+            let clause = generics.make_where_clause();
+            clause
+                .predicates
+                .push_value(parse_quote! { #ty_n: ::std::convert::Into<#ty> });
         } else {
             types.push(ty.to_token_stream());
         }
@@ -57,21 +68,19 @@ fn generate_from_variant(type_ident: &Ident, variant: &Variant) -> syn::Result<T
         bind_idents.push(bind_ident);
     }
 
-    let where_constraint = if gen_into {
-        Some(quote! { where #(#type_constraints),* })
-    } else {
-        None
-    };
-    let type_declarations = if gen_into {
-        Some(quote! { <#(#types),*> })
-    } else {
-        None
-    };
-    let types = quote! { (#(#types),*) };
+    let from_types = quote! { (#(#types),*) };
+    let mut impl_types: Vec<_> = generics
+        .params
+        .iter()
+        .map(|param| param.to_token_stream())
+        .collect();
+    if gen_into {
+        impl_types.extend(types);
+    }
 
     Ok(quote! {
-        impl #type_declarations ::std::convert::From<#types> for #type_ident #where_constraint {
-            fn from((#(#bind_idents),*): #types) -> Self {
+        impl<#(#impl_types),*> ::std::convert::From<#from_types> for #type_ident#generics {
+            fn from((#(#bind_idents),*): #from_types) -> Self {
                 Self::#variant_ident(#(#use_idents),*)
             }
         }
@@ -80,13 +89,13 @@ fn generate_from_variant(type_ident: &Ident, variant: &Variant) -> syn::Result<T
 
 pub fn generate_from_variants(input: DeriveInput) -> syn::Result<TokenStream> {
     let type_ident = &input.ident;
-    Ok(match input.data {
+    Ok(match &input.data {
         Data::Enum(data) => data
             .variants
             .iter()
             .filter_map(|variant| {
                 if matches!(variant.fields, Fields::Unnamed(_)) {
-                    Some(generate_from_variant(type_ident, variant))
+                    Some(generate_from_variant(&input, type_ident, variant))
                 } else {
                     None
                 }
